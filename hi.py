3af48336e4dec52f62e2db958a128357928c86da
Alberto Ortiz <65422300+tizor98@users.noreply.github.com>
2023-09-17 16:40:32 -0500

    minor fixed

--- hi.py
+++ hi.py
@@ -1,9 +1,8 @@
 import os
-import time
 
 def main():
    user = os.getenv("USERNAME") if os.getenv("USERNAME") is not None else "visitor"
-   print(f"Hi {user} from a Github Actions worflow")
+   print(f"Hi {user} from a Github Actions workflow")
 
 if __name__ == "__main__":
    main()

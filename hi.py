a42822c48c5ddc4a2612464d55b5db659bfe629a
Alberto Ortiz <65422300+tizor98@users.noreply.github.com>
2023-09-17 16:38:33 -0500

    extend python script

--- hi.py
+++ hi.py
@@ -1,1 +1,9 @@
-print("Hi visitor")
+import os
+import time
+
+def main():
+   user = os.getenv("USERNAME") if os.getenv("USERNAME") is not None else "visitor"
+   print(f"Hi {user} from a Github Actions worflow")
+
+if __name__ == "__main__":
+   main()
